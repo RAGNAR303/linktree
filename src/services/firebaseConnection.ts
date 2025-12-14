6bbb881b6b0b62a56f3e55147c6bdfb1962d84cd
RAGNAR303 <goncalves303.gt@gmail.com>
2025-12-14 17:51:29 -0300

    config .env firebase

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -3,14 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore'
 
 
+
 const firebaseConfig = {
- apiKey: "AIzaSyBMSXroV5MfHicVAERTgmcsk1XtVeOZ30g",
-  authDomain: "react-liks.firebaseapp.com",
-  projectId: "react-liks",
-  storageBucket: "react-liks.firebasestorage.app",
-  messagingSenderId: "686610457222",
-  appId: "1:686610457222:web:cb03052c842ef50cf86707",
-  measurementId: "G-LTPFXPNPV0"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN ,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
